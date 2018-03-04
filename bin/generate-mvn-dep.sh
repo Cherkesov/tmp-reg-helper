5a0fc58907107d5ba3a7dd667e796961dbfe46f8
Sergey Cherkesov <go.for.broke1006@gmail.com>
2018-03-04 13:41:39 +0300

    Refactoring

--- bin/generate-mvn-dep.sh
+++ bin/generate-mvn-dep.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
+
 mvn install:install-file \
 	-DgroupId=com.racoonberus.tplRegHelper \
 	-DartifactId=tplRegHelper \
-	-Dversion=1.0 \
+	-Dversion=1.0.1 \
 	-Dfile=target/tpl-reg-helper-1.0-SNAPSHOT.jar \
 	-Dpackaging=jar \
 	-DgeneratePom=true \
